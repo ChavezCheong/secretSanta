9c056a04db445a2df10810726540d29d76dd7451
Jennifer Guo <jenniferguo100@gmail.com>
2020-12-05 21:06:07 -0500

    homepage

--- src/Pages/Components/ReceiverCard/ReceiverCard.js
+++ src/Pages/Components/ReceiverCard/ReceiverCard.js
@@ -1,8 +1,11 @@
 import React from 'react';
 import {Link} from 'react-router-dom';
 import './ReceiverCard.css';
-import logo from '../../../Images/logo.svg';
-import { Card } from 'react-bootstrap';
+import pic0 from '../../../Images/snowman.svg';
+import pic1 from '../../../Images/penguin.svg';
+import pic2 from '../../../Images/reindeer.svg';
+import pic3 from '../../../Images/nutcracker.svg';
+import { Card, Button, Row, Col } from 'react-bootstrap';
 import {firebaseConnect } from 'react-redux-firebase';
 import {connect} from 'react-redux';
 import {compose} from 'redux';
@@ -12,11 +15,27 @@ const receivercard = (props) => {
   const name = props.name;
   const bio = props.bio;
 
+  const arr = [pic0, pic1, pic2, pic3];
+  const rand = Math.floor(Math.random() * Math.floor(4));
+  const img = arr[rand];
+
   return (
-    <Card className='receivercard cinzel'>
-      {name}
-      <br/>
-      {bio}
+    <Card bg='light' className='receivercard'>
+      <Row>
+        <Col xs={2}>
+          <img src={img} style={{margin: '1em', borderRadius: '50%', width: '10em'}}/>
+        </Col>
+        <Col>
+          <Card.Body>
+            <h1 className='cinzel'>{name}</h1>
+            <hr/>
+            <Card.Title>
+              <p className='raleway'>{bio}</p>
+            </Card.Title>
+            <Button variant="dark">View Profile Here</Button>
+          </Card.Body>
+        </Col>
+      </Row>
     </Card>
   );
 }
