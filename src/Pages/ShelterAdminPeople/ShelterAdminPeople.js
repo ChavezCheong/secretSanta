e0c8269a99ace678d5e7eca57e05ef57f2be170c
blokus2015 <seanroades@gmail.com>
2020-12-05 19:37:11 -0500

    added blank pages for the shelter admin payments

---
+++ src/Pages/ShelterAdminPeople/ShelterAdminPeople.js
@@ -0,0 +1,11 @@
+import React, {Component} from 'react';
+
+class ShelterAdminPeople extends Component {
+  render() {
+    return (
+      <p>People</p>
+    )
+  }
+}
+
+export default ShelterAdminPeople;
--- src/Routes/Routes.js
+++ src/Routes/Routes.js
@@ -3,6 +3,9 @@ import { Switch, Route } from 'react-router-dom';
 
 import ShelterRegister from '../Pages/ShelterRegister/ShelterRegister'
 import ShelterLogin from '../Pages/ShelterLogin/ShelterLogin'
+import ShelterAdminPeople from '../Pages/ShelterAdminPeople/ShelterAdminPeople';
+import ShelterAdminPayments from '../Pages/ShelterAdminPayments/ShelterAdminPayments';
+
 
 const Routes = () => {
   return (
@@ -13,6 +16,12 @@ const Routes = () => {
       </Route>
       <Route exact path="/login">
         <ShelterLogin/>
+      </Route>
+      <Route exact path="/ShelterAdminPeople">
+        <ShelterAdminPeople/>
+      </Route>
+      <Route exact path="/ShelterAdminPayments">
+        <ShelterAdminPayments/>
       </Route>
     </Switch>
     </>
